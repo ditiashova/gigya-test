d9d03ed0b4ff72d6acca10903b74196685fb4aa5
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-04-19 12:30:56 +0300

    GG-7: added interceptor block

---
+++ src/app/core/auth.interceptor.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest, JsonpInterceptor} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {BrowserStandardEncoder} from './core.utils';
+
+const USER_KEY = 'AJA3Cw9XcJZf';
+const USER_SECRET = '1J+YxAY47khnuXf4GKSggLpPFBbQv8Hq';
+const API_KEY = '3_inujb44QPskKBok5VwhYnqy40eaVrwAJXXLsqaHRI_6DCM3KHhxNXjjcFQe0PASK';
+
+@Injectable()
+export class AuthInterceptor extends JsonpInterceptor {
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authParams = new HttpParams({
+      encoder: new BrowserStandardEncoder() // Fix for https://github.com/angular/angular/issues/18884
+    })
+      .set('secret', USER_SECRET)
+      .set('apikey', API_KEY)
+      .set('userkey', USER_KEY);
+
+    return next.handle(request.clone({params: authParams}));
+  }
+}
